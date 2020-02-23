0cfac86edf61e46f6aa42dd562b588319ec528c9
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-02-23 18:35:37 -0500

    variables stated and for loop added

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function updateInput(val) {
     document.getElementById("textInput").value = val;
 }
+// the scrambler
+var digt = 8
+var low
+var upp
+var num
+var spe
+
+for (var digt; digt < 0; digt--) {
+    
+    
+}
