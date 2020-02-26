77ee2f95f512a1538641d967a748bc06e100f299
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-02-26 18:21:29 -0500

    added function to check if password contains at least one of the selected characters

--- script2.js
+++ script2.js
@@ -1,18 +1,19 @@
 // strings and password
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "1234567890";
-var special = "!@#$%^&*()_+=<>?/";
+var number = "12345678901234567890123456";
+var special = "!@#$%^&*()_+=<>?/!@#$%^&*(";
 var masterString = "";
 var password = "";
 var goodPass = 0
 //user input values
 var dig = 8
 var low = 1
-var upp = 0
+var upp = 1
 var num = 1
 var spe = 0
-
+var total = low + upp + num + spe
+function passwordGen(){
 // string combiner
 if (low === 1) {
     masterString += lower
@@ -26,27 +27,30 @@ if (num === 1) {
 if (spe === 1) {
     masterString += special
 }
-//while loop 
+//while loop selects one random character from MASTERSTRING and adds it to PASSWORD. Loop stops when PASSWORD string length = DIG
 while (password.length < dig) {
     password += masterString[Math.floor(Math.random() * masterString.length)]
 }
 //check if contains
-if (low === 1) {
-    
+if ( inPassword(lower) + inPassword(upper) + inPassword(number) + inPassword(special) === total ) {
+    console.log(password)
 }
-function inPassword() {
-    for (var i = 0; i < lower.length; i++) {
-        var contain = password.includes(lower[i])
+
+
+function inPassword(string) {
+    for (var i = 0; i < string.length; i++) {
+        var contain = password.includes(string[i])
         if (contain) {
-            i = lower.length;
+            i = string.length;
+            return 1
         }
-        if (!contain && i === lower.length){
-            goodPass++
-        }
+
+
     }
 }
-    
 
-console.log(password)
+}
+
+
 
 
